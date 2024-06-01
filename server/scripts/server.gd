086665a1b0d02ceab0f4a6d6a700bc170e27e83a
Jean-Marc van Leerdam <jmvanleerdam@gmail.com>
2024-06-01 21:10:30 +0200

    Add HttpFileRouter to the server, to serve the webapp.

--- server/scripts/server.gd
+++ server/scripts/server.gd
@@ -1,8 +1,10 @@
 extends Node
 
 func _ready():
+	var webappServer = HttpFileRouter.new("../exports")
+
 	var server = HttpServer.new()
 	server.register_router("/fib", FibonacciRouter.new())
+	server.register_router("/app", webappServer)
 	add_child(server)
 	server.start()
-
