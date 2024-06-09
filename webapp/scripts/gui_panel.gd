6e4d2600adc440d35c7210c1a4ca6a73aec7d75d
Jean-Marc van Leerdam <jmvanleerdam@gmail.com>
2024-06-09 10:18:45 +0200

    Add world objects that support collision

--- webapp/scripts/gui_panel.gd
+++ webapp/scripts/gui_panel.gd
@@ -6,7 +6,7 @@ extends Node3D
 func _ready():
 	var gui = preload("res://scenes/metrics_gui.tscn")
 	var guiInstance = gui.instantiate()
-	$SubViewport.add_child(guiInstance)
+	subviewport.add_child(guiInstance)
 	pass # Replace with function body.
 
 
